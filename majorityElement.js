4fcef2a547636aae75544396d836724355614651
Ashby Gunter <theshbee@yahoo.com>
2020-05-06 10:12:31 -0700

    Add testing to majorityElement

--- majorityElement.js
+++ majorityElement.js
@@ -40,3 +40,40 @@ var majorityElement = function(nums) {
     // else
       // give it a count of 1
 };
+
+// testing
+var test1 = [1];
+result1 = majorityElement(test1);
+console.log(result1); // 1
+
+var test2 = [2, 2, 3];
+result2 = majorityElement(test2);
+console.log(result2);  //
+
+var test3 = [4, 5, 5];
+result3 = majorityElement(test3);
+console.log(result3);  // 5
+
+var test4 = [6, 7, 6, 6];
+result4 = majorityElement(test4);
+console.log(result4);  // 6
+
+var test5 = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8];
+result5 = majorityElement(test5);
+console.log(result5);  // 8
+
+var test6 = [10, 9, 11, 9, 12, 9, 13, 9, 14, 9, 9];
+result6 = majorityElement(test6);
+console.log(result6);  // 9
+
+var test7 = [3,2,3];
+result7 = majorityElement(test7);
+console.log(result7);  // 3
+
+var test8 = [2,2,1,1,1,2,2];
+result8 = majorityElement(test8);
+console.log(result8);  // 2
+
+// var test9 = ;
+// result9 = majorityElement(test9);
+// console.log(result9);
